1031ba97882efd5eb8b3c8c5dfee5d8661c207ac
KarolSU96 <karol96266@gmail.com>
2023-07-12 16:17:59 +0200

    Add eventlistener to Add button + function that checks if input is valid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-let task = document.getElementById("new-task").value;
+let input = document.getElementById("new-task");
 let addButton = document.getElementById("add-button");
+let ul = document.getElementsByTagName("ul");
 
 addButton.addEventListener("click", function () {
-  alert("You clicked add button");
+  let task = input.value;
+
+  if (!task) {
+    alert("Please enter your tast before adding it");
+  } else {
+    let li = document.createElement("li");
+
+    li.textContent = task;
+
+    ul[0].appendChild(li);
+
+    input.value = "";
+  }
 });
