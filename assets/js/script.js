adaea457c474919834c45c3f4bbdab386bdd063a
KarolSU96 <karol96266@gmail.com>
2023-07-12 13:12:24 +0200

    Add maxlenght to input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 let task = document.getElementById("new-task").value;
 let addButton = document.getElementById("add-button");
 
-addButton.addEventListener("click", function())
+addButton.addEventListener("click", function () {
+  alert("You clicked add button");
+});
