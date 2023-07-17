ecbc1651dad404d6a3d9b36769cad8c3dd4ddd2b
KarolSU96 <karol96266@gmail.com>
2023-07-17 10:03:35 +0200

    Add style class 'completed-task' for line though styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ addButton.addEventListener("click", function () {
   }
 });
 
-/* deleteting the notes. Method searsched for class "delete-button"
+/* deleteting the notes. Method searches for class "delete-button"
  if the element with this class is clicked it removes its parent li element. */
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("delete-button")) {
